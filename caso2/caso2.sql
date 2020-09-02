2c0de80d6cf60bc93592d188d74567bf27b209f6
maria alvarez <mar.alvareg@gmail.com>
2020-09-02 18:23:09 -0400

    parentesis

--- caso2/caso2.sql
+++ caso2/caso2.sql
@@ -2,5 +2,5 @@ CREATE DATABASE caso2;
 CREATE TABLE cursos (id SERIAL PRIMARY KEY, nombre VARCHAR(25));
 CREATE TABLE alumnos(rut VARCHAR(10) PRIMARY KEY, nombre VARCHAR(50), id_curso INT, FOREIGN KEY(id_curso) REFERENCES cursos(id));
 CREATE TABLE departamentos(id SERIAL PRIMARY KEY, nombre VARCHAR(25));
-CREATE TABLE profesores (id SERIAL PRIMARY KEY, nombre VARCHAR(25), id_dpto INT, FOREIGN KEY id_dpto REFERENCES departamentos(id));
-CREATE TABLE pruebas (id SERIAL PRIMARY KEY, puntaje INT, a_rut VARCHAR(10), p_id INT, FOREIGN KEY a_rut REFERENCES alumnos(rut), FOREIGN KEY p_id REFERENCES profesores(id));
+CREATE TABLE profesores (id SERIAL PRIMARY KEY, nombre VARCHAR(25), id_dpto INT, FOREIGN KEY (id_dpto) REFERENCES departamentos(id));
+CREATE TABLE pruebas (id SERIAL PRIMARY KEY, puntaje INT, a_rut VARCHAR(10), p_id INT, FOREIGN KEY (a_rut) REFERENCES alumnos(rut), FOREIGN KEY (p_id) REFERENCES profesores(id));
